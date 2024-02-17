495140517e9b7939709f60f1531b9e59a1bd0a51
job <salhiayoub2004@gmail.com>
2024-02-17 18:30:33 +0100

    3.20 finished

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.put("/api/persons/:id",(request, response, next) => {
   const person = {
     number: body.number
   }
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {  new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
--- modules/persone.js
+++ modules/persone.js
@@ -1,39 +1,46 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
+const url = process.env.MONGODB_URI;
 
-const url = process.env.MONGODB_URI
+console.log("connecting to", url);
 
-console.log('connecting to', url)
+mongoose
+  .connect(url)
 
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-  const personSchema = new mongoose.Schema({
-    name: {
-      type:String,
-      minLength:3
-    },
-    number: String,
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
   });
-    
-  personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+  },
+  number: { 
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function validatePhoneNumber(phoneNumber) {
+        // Regular expression to match phone numbers with format: 09-1234556
+        const phoneNumberRegex = /^\d{2,3}-\d{7,}$/;
+      
+        return phoneNumberRegex.test(phoneNumber);
+      },
+      
+      message: props => `${props.value} is not a valid phone number!`
+    }, },
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 module.exports = mongoose.model("Person", personSchema);
-
-
-
-
